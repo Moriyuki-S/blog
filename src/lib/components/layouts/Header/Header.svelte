23853055d3cf8f01e0b09459835e71d47cd4c08e
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-16 03:40:23 +0000

    add : navを追加

--- src/lib/components/layouts/Header/Header.svelte
+++ src/lib/components/layouts/Header/Header.svelte
@@ -3,4 +3,12 @@
 
 <header>
 	<h1 class="text-lg">ブログタイトル</h1>
+	<nav class="">
+		<ul>
+			<li><a href="/">ホーム</a></li>
+			<li><a href="/">カテゴリー1</a></li>
+			<li><a href="/">カテゴリー2</a></li>
+			<li><a href="/">カテゴリー3</a></li>
+		</ul>
+	</nav>
 </header>
