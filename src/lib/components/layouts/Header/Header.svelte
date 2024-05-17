ea827a19df81c25bb279b025198fe3908bfc6051
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-17 10:37:34 +0000

    add : navにaria-labelledby属性をつけた

--- src/lib/components/layouts/Header/Header.svelte
+++ src/lib/components/layouts/Header/Header.svelte
@@ -8,7 +8,7 @@
     >
         ブログタイトル
     </h1>
-    <nav class="hidden md:flex justify-center items-center h-10 *:h-full bg-sky-50">
+    <nav aria-labelledby="desktop-navigation" class="hidden md:flex justify-center items-center h-10 *:h-full bg-sky-50">
         <ul class="flex *:border-r-2 *:w-28 *:text-center *:h-full first:border-l-2 ">
             <li><a href="/" class="inline-block w-full h-full hover:bg-sky-200"><span class="leading-10 text-sky-900">カテゴリー1</span></a></li>
             <li><a href="/" class="inline-block w-full h-full hover:bg-sky-200"><span class="leading-10 text-sky-900">カテゴリー2</span></a></li>
