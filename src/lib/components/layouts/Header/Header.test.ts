1ea3a2594bc5d37cd899d5bec55a276c05ba84fd
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-17 10:37:13 +0000

    add : Headerのテストコードを実装

---
+++ src/lib/components/layouts/Header/Header.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, it } from 'vitest';
+import { render, screen } from '@testing-library/svelte';
+import Header from './Header.svelte';
+
+describe('ヘッダーのビジュアルテスト', () => {
+	describe('ビューポートに無関係なテスト', () => {
+        it('ヘッダータグが存在する', () => {
+            render(Header);
+            const header = screen.queryByRole('banner');
+            expect(header).toBeInTheDocument();
+        });
+
+        it('ヘッダーのタイトルが存在する', () => {
+            render(Header);
+            const title = screen.queryByRole('heading');
+            expect(title).toBeInTheDocument();
+        });
+    });
+});
+
+describe('ヘッダーのアクセシビリティテスト', () => {
+    it('ナビゲーションに指定のaria-labeledby属性がつけられている', () => {
+        render(Header);
+        const label = 'desktop-navigation';
+        const nav = screen.getByRole('navigation');
+        expect(nav).toHaveAttribute('aria-labelledby', label);
+    });
+});
