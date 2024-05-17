b2d3506edf25b0d85d5f224cdb52e04e92164993
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-17 09:51:58 +0000

    add : ヘッダーを作成
    デザイン要検討

--- src/lib/components/layouts/Header/Header.svelte
+++ src/lib/components/layouts/Header/Header.svelte
@@ -2,13 +2,17 @@
 </script>
 
 <header>
-	<h1 class="text-lg">ブログタイトル</h1>
-	<nav class="">
-		<ul>
-			<li><a href="/">ホーム</a></li>
-			<li><a href="/">カテゴリー1</a></li>
-			<li><a href="/">カテゴリー2</a></li>
-			<li><a href="/">カテゴリー3</a></li>
-		</ul>
-	</nav>
+    <h1
+        class="w-full h-10 leading-10 pl-4 text-xl border-b
+               md:h-20 md:text-3xl md:text-center md:border-none"
+    >
+        ブログタイトル
+    </h1>
+    <nav class="hidden md:flex justify-center items-center h-10 *:h-full bg-sky-50">
+        <ul class="flex *:border-r-2 *:w-28 *:text-center *:h-full first:border-l-2 ">
+            <li><a href="/" class="inline-block w-full h-full hover:bg-sky-200"><span class="leading-10 text-sky-900">カテゴリー1</span></a></li>
+            <li><a href="/" class="inline-block w-full h-full hover:bg-sky-200"><span class="leading-10 text-sky-900">カテゴリー2</span></a></li>
+            <li><a href="/" class="inline-block w-full h-full hover:bg-sky-200"><span class="leading-10 text-sky-900">カテゴリー3</span></a></li>
+        </ul>
+    </nav>
 </header>
