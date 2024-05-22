aa9a69d0c0dca8edb05b8af7952071b6d7ddaff7
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-22 23:24:16 +0000

    add : articleにslugとabstractも追加

--- src/lib/features/article/types/type.ts
+++ src/lib/features/article/types/type.ts
@@ -2,7 +2,9 @@ import type { Tag } from "$lib/features/tags/types/type";
 
 export type Article = {
     id: string;
+    slug: string;
     title: string;
+    abstract: string;
     content: string;
     imageUrl: string;
     createdAt: string;
