bf8b19fe7db66952121dbf243a93becf25891676
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-22 11:44:01 +0000

    fix : フォーマットを修正

--- src/lib/components/layouts/Footer/Footer.test.ts
+++ src/lib/components/layouts/Footer/Footer.test.ts
@@ -1,11 +1,11 @@
-import { render, screen } from "@testing-library/svelte";
-import { Footer } from "flowbite-svelte";
-import { describe, expect, it } from "vitest";
+import { render, screen } from '@testing-library/svelte';
+import { Footer } from 'flowbite-svelte';
+import { describe, expect, it } from 'vitest';
 
 describe('FooterのUIテスト', () => {
-    it('Footerが表示される', () => {
-        render(Footer);
-        const footer = screen.queryByRole('contentinfo');
-        expect(footer).toBeInTheDocument();
-    });
+	it('Footerが表示される', () => {
+		render(Footer);
+		const footer = screen.queryByRole('contentinfo');
+		expect(footer).toBeInTheDocument();
+	});
 });
