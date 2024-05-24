c0ede9072040a54fd5076f40192ffd492574fa70
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-24 15:46:21 +0000

    add : ArticleDetailの処理の一部を追加

---
+++ src/lib/features/article/components/ArticleDetail/ArticleDetail.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import type { Article } from "../../types/type";
+
+    export let article: Readonly<Article>;
+</script>
+
+<article class="w-full">
+
+    <h2>{article.title}</h2>
+    <div>
+        <img src={article.imageUrl} alt={`「${article.title}」のイメージ画像`}>
+    </div>
+    <div>
+        {article.content}
+    </div>
+
+
+</article>
