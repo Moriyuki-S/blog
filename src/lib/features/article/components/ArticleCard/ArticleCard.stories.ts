5bf85413f6189c417e0c16775d96bed159c44f95
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-24 10:24:30 +0000

    add : タグのチップを追加

--- src/lib/features/article/components/ArticleCard/ArticleCard.stories.ts
+++ src/lib/features/article/components/ArticleCard/ArticleCard.stories.ts
@@ -15,6 +15,20 @@ export const Default: Story = {
 			title: 'サンプル記事',
 			imageUrl: 'https://www.pokemon.co.jp/PostImages/d86fdbe4e3d1e9d680b5217f7d947425caae82aa.jpg',
 			body: 'これはサンプルの記事です。dfadfasdfasdfadf',
+			tags: [
+				{
+					id: '1',
+					name: 'サンプル',
+					iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
+					color: '#ff0000'
+				},
+				{
+					id: '2',
+					name: 'プログラミング',
+					iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
+					color: '#ff0000'
+				}
+			],
 			createdAt: '2024-05-22T23:20:33.446Z',
 			updatedAt: '2024-05-22T23:20:33.446Z'
 		}
