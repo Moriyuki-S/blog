3613b69306b07be5a42e623387f4df042fb317ab
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-24 10:11:48 +0000

    format : フォーマットを修正

--- src/lib/features/tag/components/TagChip/TagChip.stories.ts
+++ src/lib/features/tag/components/TagChip/TagChip.stories.ts
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/svelte";
-import TagChip from "./TagChip.svelte";
+import type { Meta, StoryObj } from '@storybook/svelte';
+import TagChip from './TagChip.svelte';
 
 const meta: Meta = {
-    component: TagChip
+	component: TagChip
 };
 
 export default meta;
@@ -10,12 +10,12 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        tag: {
-            id: '1',
-            name: 'プログラミング',
-            iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
-            color: '#FF0000'
-        }
-    }
+	args: {
+		tag: {
+			id: '1',
+			name: 'プログラミング',
+			iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
+			color: '#FF0000'
+		}
+	}
 };
--- src/lib/features/tag/components/TagChip/TagChip.test.ts
+++ src/lib/features/tag/components/TagChip/TagChip.test.ts
@@ -1,18 +1,18 @@
-import { describe, expect, it } from "vitest";
-import type { Tag } from "../../types/type";
-import { render, screen } from "@testing-library/svelte";
-import TagChip from "./TagChip.svelte";
+import { describe, expect, it } from 'vitest';
+import type { Tag } from '../../types/type';
+import { render, screen } from '@testing-library/svelte';
+import TagChip from './TagChip.svelte';
 
 describe('タグのチップコンポーネントのテスト', () => {
-    it('タグの名前が表示されること', () => {
-        const TAG: Tag = {
-            id: '1',
-            name: 'サンプル',
-            iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
-            color: '#ff0000',
-        };
-        render(TagChip, { tag: TAG });
-        const tagName = screen.queryByText(TAG.name);
-        expect(tagName).toBeInTheDocument();
-    });
-})
+	it('タグの名前が表示されること', () => {
+		const TAG: Tag = {
+			id: '1',
+			name: 'サンプル',
+			iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
+			color: '#ff0000'
+		};
+		render(TagChip, { tag: TAG });
+		const tagName = screen.queryByText(TAG.name);
+		expect(tagName).toBeInTheDocument();
+	});
+});
