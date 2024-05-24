de054654bca9920cf98bd9417142f049e06b57dc
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-24 15:57:15 +0000

    format : フォーマットを正した

--- src/lib/features/article/components/ArticleCard/ArticleCard.test.ts
+++ src/lib/features/article/components/ArticleCard/ArticleCard.test.ts
@@ -18,7 +18,7 @@ describe('記事のカードコンポーネントのテスト', () => {
 				id: '1',
 				name: 'サンプルタグ',
 				iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
-				color: '#ff0000',
+				color: '#ff0000'
 			}
 		};
 		render(ArticleCard, { article: SAMPLE_ARTICLE });
@@ -34,12 +34,12 @@ describe('記事のカードコンポーネントのテスト', () => {
 			content: 'これはテストの記事です。',
 			slug: '/',
 			createdAt: dayjs().subtract(1, 'day').toDate(),
-			updatedAt: dayjs().subtract(1, 'day').toDate(), 
+			updatedAt: dayjs().subtract(1, 'day').toDate(),
 			tag: {
 				id: '1',
 				name: 'サンプルタグ',
 				iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
-				color: '#ff0000',
+				color: '#ff0000'
 			}
 		};
 
@@ -61,8 +61,8 @@ describe('記事のカードコンポーネントのテスト', () => {
 				id: '1',
 				name: 'サンプルタグ',
 				iconUrl: 'https://icon-pit.com/wp-content/uploads/2018/10/note-pc_icon_79.png',
-				color: '#ff0000',
-			},
+				color: '#ff0000'
+			}
 		};
 		render(ArticleCard, { article: SAMPLE_ARTICLE });
 		const date = screen.getByTestId('article-card-time');
