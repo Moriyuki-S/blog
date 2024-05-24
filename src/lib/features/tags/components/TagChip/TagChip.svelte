f03f1a67c8603a29fc5a53892dbc1a914b08ea8e
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-24 09:04:45 +0000

    add : chipのhtmlの構造とstoryを追加

---
+++ src/lib/features/tags/components/TagChip/TagChip.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import type { Tag } from "../../types/type";
+
+    export let tag: Readonly<Tag>;
+</script>
+
+<div class="w-5 h-3 border">
+    <img src={tag.iconUrl} alt={`${tag.name}のアイコン`}>
+    <span>{tag.name}</span>
+</div>
