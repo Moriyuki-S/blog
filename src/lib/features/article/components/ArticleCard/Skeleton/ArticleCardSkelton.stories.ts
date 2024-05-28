3797053a9cbcb435305d7aa50bb644e2a2bb0a46
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-28 16:30:13 +0000

    format : フォーマットを修正

--- src/lib/features/article/components/ArticleCard/Skeleton/ArticleCardSkelton.stories.ts
+++ src/lib/features/article/components/ArticleCard/Skeleton/ArticleCardSkelton.stories.ts
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/svelte";
-import ArticleCardSkelton from "./ArticleCardSkelton.svelte";
+import type { Meta, StoryObj } from '@storybook/svelte';
+import ArticleCardSkelton from './ArticleCardSkelton.svelte';
 
 const meta: Meta = {
-    component: ArticleCardSkelton
+	component: ArticleCardSkelton
 };
 
 export default meta;
