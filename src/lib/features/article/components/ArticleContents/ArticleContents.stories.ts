30705667b555e9d4f2066e2d1259e085153e9617
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-28 10:59:32 +0000

    format  : フォーマットを修正

--- src/lib/features/article/components/ArticleContents/ArticleContents.stories.ts
+++ src/lib/features/article/components/ArticleContents/ArticleContents.stories.ts
@@ -1,8 +1,8 @@
-import type { Meta, StoryObj } from "@storybook/svelte";
-import ArticleContents from "./ArticleContents.svelte";
+import type { Meta, StoryObj } from '@storybook/svelte';
+import ArticleContents from './ArticleContents.svelte';
 
 const meta: Meta = {
-    component: ArticleContents
+	component: ArticleContents
 };
 
 export default meta;
@@ -10,19 +10,19 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-    args: {
-        article: {
-            id: '1',
-            slug: 'test-article',
-            title: 'Test Article',
-            body: '<h2>Heading 1</h2><p>Paragraph 1</p><h3>Heading 1-1</h3><p>Paragraph 1-1</p><h2>Heading 2</h2><p>Paragraph 2</p>',
-            imageUrl: 'https://placehold.jp/300x300.png',
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            tag: {
-                id: '1',
-                name: 'Test Tag'
-            }
-        }
-    }
+	args: {
+		article: {
+			id: '1',
+			slug: 'test-article',
+			title: 'Test Article',
+			body: '<h2>Heading 1</h2><p>Paragraph 1</p><h3>Heading 1-1</h3><p>Paragraph 1-1</p><h2>Heading 2</h2><p>Paragraph 2</p>',
+			imageUrl: 'https://placehold.jp/300x300.png',
+			createdAt: new Date(),
+			updatedAt: new Date(),
+			tag: {
+				id: '1',
+				name: 'Test Tag'
+			}
+		}
+	}
 };
