4b5210943d9128764dea0db058cbe557e6a1f30c
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-28 14:43:03 +0000

    fix : styleClassをオプショナルにした

--- src/lib/components/layouts/RightSidebar/RightSidebar.svelte
+++ src/lib/components/layouts/RightSidebar/RightSidebar.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	export let styleClass: string;
+	export let styleClass: string = '';
 </script>
 
 <aside class={`hidden md:block max-w-72 border border-sky-200 ${styleClass}`}>
