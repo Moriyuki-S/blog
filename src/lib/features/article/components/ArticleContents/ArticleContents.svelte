57996039c4fa61fb2b65b3ac8281d00c8be77d86
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-28 10:53:52 +0000

    add : tocbotで目次を作成

--- src/lib/features/article/components/ArticleContents/ArticleContents.svelte
+++ src/lib/features/article/components/ArticleContents/ArticleContents.svelte
@@ -2,20 +2,24 @@
 	import type { Article } from '../../types/type';
 	import { extractHeadings } from '../../logics/parse-html';
 	import { Timeline, TimelineItem } from 'flowbite-svelte';
+	import { onMount } from 'svelte';
+	import tocbot from 'tocbot';
 
-	export let article: Article;
-	$: tableOfContents = extractHeadings(article.body);
+	onMount(() => {
+		tocbot.init({
+			tocSelector: '.toc',
+			contentSelector: '.article',
+			extraLinkClasses: 'block w-max h-10 hover:bg-slate-100 rounded-lg px-4 py-1',
+			activeLinkClass: 'text-sky-500',
+			headingSelector: 'h2, h3'
+		});
+
+		return tocbot.destroy;
+	});
 </script>
 
 <div class="w-full max-w-64 h-96 pl-5 pt-4 rounded-md overflow-y-scroll">
 	<h4 class="text-xl font-bold">目次</h4>
-	<div class="mt-5">
-		<Timeline>
-			{#each tableOfContents as content}
-				<TimelineItem>
-                    <a href="/" class={(content.tagName === 'H2' ? "font-bold " : "") + "block box-border w-fit rounded-full px-3 py-1 -translate-y-1 hover:bg-slate-100"}>{content.text}</a>
-				</TimelineItem>
-			{/each}
-		</Timeline>
-	</div>
+	<nav class="toc mt-5">
+	</nav>
 </div>
