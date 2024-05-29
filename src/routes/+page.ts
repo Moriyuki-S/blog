051df934da6ba4692113c466843669216670f62d
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-29 15:04:10 +0000

    add : CSRでブックマークした記事を取得する

---
+++ src/routes/+page.ts
@@ -0,0 +1,11 @@
+import { ArticleAPIs } from '$lib/features/article/repositories/apis/fetch-articles';
+import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ data }) => {
+    const bookmarkedArticles = await ArticleAPIs.fetchBookmarkedArticles();
+
+    return {
+        ...data,
+        bookmarkedArticles
+    };
+};
