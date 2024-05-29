16f49dedef9127eba42e86a2a63f3f00f293c2b6
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-29 11:49:33 +0000

    remove : 余計なpage.tsを削除

--- src/routes/+page.ts
+++
@@ -1,9 +0,0 @@
-import { fetchArticles } from '$lib/features/article/repositories/apis/fetch-articles';
-import type { PageLoad } from './$types';
-
-export const load: PageLoad = async () => {
-    const popularArticles = await fetchArticles.fetchPopularArticles();
-    return {
-        popularArticles
-    };
-};
