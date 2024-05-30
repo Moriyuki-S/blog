79978c9d1eab888a190d3bafa2431468110d5194
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-05-30 09:27:15 +0000

    test : プロフィール表示のテストを実行

---
+++ src/lib/features/user/components/UserProfileCard/UserProfileCard.test.ts
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/svelte";
+import { describe, expect, it } from "vitest";
+import UserProfileCard from "./UserProfileCard.svelte";
+import type { Administrator } from "../../types/type";
+
+describe('ユーザープロフィールのUIテスト', () => {
+    const ADMINISTRATOR: Administrator = {
+        name: 'サンプルユーザー',
+        biography: 'サンプルのユーザーです',
+        iconUrl: ''
+    };
+
+    it('ユーザーの名前が表示される', () => {
+        render(UserProfileCard, { user: ADMINISTRATOR });
+        const name = screen.getByText(ADMINISTRATOR.name);
+        expect(name).toBeInTheDocument();
+    });
+
+    it('ユーザーの自己紹介文が表示される', () => {
+        render(UserProfileCard, { user: ADMINISTRATOR });
+        const biography = screen.getByText(ADMINISTRATOR.biography);
+        expect(biography).toBeInTheDocument();
+    });
+
+    it('「詳しく見るボタン」が表示される', () => {
+        render(UserProfileCard, { user: ADMINISTRATOR });
+        const button = screen.getByRole('button', { name: '詳しく見る' });
+        expect(button).toBeInTheDocument();
+    });
+});
