e687851672d720628446d885afdce6c230bc837d
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-02 06:17:53 +0000

    feat : タグによる検索結果画面を作成

---
+++ src/routes/tags/[slug]/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import ArticleVerticalCard from '$lib/features/article/components/ArticleVerticalCard/ArticleVerticalCard.svelte';
+import ArticleVerticalCardSkeleton from '$lib/features/article/components/ArticleVerticalCard/Skeleton/ArticleVerticalCardSkeleton.svelte';
+	import { RefreshOutline } from 'flowbite-svelte-icons';
+import type { PageData } from './$types';
+	import { Tooltip } from 'flowbite-svelte';
+
+    export let data: PageData;
+</script>
+
+<main class="pt-16 px-4">
+    <h1 class="text-3xl text-center">
+        タグ : {data.tag.name}
+        <button id="refresh-tag-button" class="w-fit h-fit p-2 rounded-full hover:bg-slate-200"><RefreshOutline /></button>
+        <Tooltip triggeredBy="#refresh-tag-button" placement="right">
+            変更する
+        </Tooltip>
+    </h1>
+    <ul class="grid grid-cols-3 gap-5 mt-10">
+        {#await data.articles}
+            {#each Array(8) as _}
+                <ArticleVerticalCardSkeleton />
+            {/each}
+        {:then articles} 
+            {#each articles as article}
+                <ArticleVerticalCard {article} />
+            {/each}
+        {/await}
+    </ul>
+</main>
