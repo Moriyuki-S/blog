e685141fb282014ec2608e3f606284804d5d5cbd
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-06 12:44:29 +0000

    feat : TagCardコンポーネントを作成

---
+++ src/lib/features/tag/components/TagCard/TagCard.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import type { Tag } from "../../types/type";
+
+    export let tag: Tag;
+</script>
+
+<a href={`/tags/${tag.slug}`} class="inline-block w-fit transition-transform border border-sky-500 hover:border-sky-200 hover:bg-sky-50 hover:-translate-y-1 rounded-lg p-5">
+    <div class="flex flex-col items-center justify-center gap-y-5">
+        <div>
+            <img src={tag.iconUrl} alt="アイコン">
+        </div>
+        <h3 class="px-2 w-fit">{tag.name}</h3>
+    </div>
+</a>
