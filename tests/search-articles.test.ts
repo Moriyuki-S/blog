ff5d49bdff99d2da7cc5a340f28f46dd4d5f86b1
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-06 08:27:00 +0000

    add : タグによる検索のテストをコメントで仮に実装

--- tests/search-articles.test.ts
+++ tests/search-articles.test.ts
@@ -1,40 +1,63 @@
 import test, { expect } from '@playwright/test';
 import { SearchPage } from './pageObject/searchPage';
+import type { Tag } from '$lib/features/tag/types/type';
+// import { TaggedArticlesPage } from './pageObject/taggedArticlesPage';
 
 test.describe('記事検索機能のテスト', () => {
 	test('キーワードで記事の検索ができる', async ({ page }) => {
-
-        // 検索ページでキーワードを入力する
+		// 検索ページでキーワードを入力する
 		const searchPage = new SearchPage(page);
 		await searchPage.goto();
 		await searchPage.searchByKeyword('プログラミング');
 
-        // 検索結果ページに遷移し、検索結果が表示されていることを確認する
+		// クエリパラメータが付与され、検索した記事が表示される
+		expect(page).toHaveURL('/search?q=プログラミング');
+		
 	});
 
-    test('キーワードが空文字だとエラーが表示される', async ({ page }) => {
+	test('タグをクリックするとそのタグの付いた記事一覧のページに遷移する', async ({ page }) => {
+		const tag: Tag = {
+			'id': '1',
+			'slug': 'sample-tag',
+			'name': 'Sample Tag',
+			'iconUrl': '',
+			'color': '#000000',
+		};
 
-        // 検索ページで空文字のまま検索ボタンを押す
-        const searchPage = new SearchPage(page);
-        await searchPage.goto();
-        await searchPage.searchByKeyword('');
+		// タグをクリックしてそのタグの付いた記事一覧のページに遷移する
+		const searchPage = new SearchPage(page);
+		await searchPage.goto();
+		await searchPage.clickTag(tag.name);
 
-        // /search/resultsにリダイレクトされない
-        expect(page).toHaveURL('/search');
+		// タグのコンポーネントは未作成
 
-        // エラーメッセージが表示される
-        expect(searchPage.isErrorMessageVisible()).toBeTruthy();
+		// /tags/:tagId に遷移している
+		// expect(page).toHaveURL(`/tags/${tag.id}`);
 
-        // エラーメッセージの内容が正しいかを確認
-        expect(searchPage.errorMessage).toHaveText('キーワードを入力してください');
+		// 遷移後
+		// const taggedArticlesPage = new TaggedArticlesPage(page, tag);
+		
+		// タグ名が表示されている
+		// expect(taggedArticlesPage.tagText).toHaveText(tag.name);
+	   
+		// タグの付いた記事が表示されている
+		// expect(taggedArticlesPage.articlesList).toBeVisible();
 
-        // キーワードを一文字でも入力するとエラーメッセージが消える
-        await searchPage.searchInput.fill('a');
-        expect(await (searchPage.isErrorMessageVisible())).toBeFalsy();
+	});
 
-        // 空文字にすると再びエラーメッセージが表示される
-        await searchPage.searchInput.fill('');
-        expect(await (searchPage.isErrorMessageVisible())).toBeTruthy();
+	test('キーワードが空文字だと検索できない', async ({ page }, testInfo) => {
+		// 検索ページで空文字のまま検索ボタンを押そうとする
+		const searchPage = new SearchPage(page);
+		await searchPage.goto();
+		const currentUrl = page.url();
+		expect(searchPage.searchInput).toHaveAttribute('required');
+		await searchPage.searchByKeyword('');
 
-    });
+		// 空文字だとクエリパラメータが付与されない
+		try {
+			await page.waitForURL(currentUrl);
+		} catch (err) {
+			testInfo.fail(true, 'クエリパラメータが付与されました');
+		}
+	});
 });
