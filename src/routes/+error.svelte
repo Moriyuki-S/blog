deac51760b9d575e1fea77941302d71938a02822
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-08 03:45:49 +0000

    rename : 綴りミスを修正

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<main>
+    <picture>
+        <source srcset="">
+    </picture>
+    <h1>{$page.status}</h1>
+    <p>{$page.error?.message}</p>
+</main>
