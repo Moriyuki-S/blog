bf5a0e7459af6d1367e1cd712077d42d06d05e03
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-08 11:59:58 +0000

    add : エラークラスのベースを作成

---
+++ src/lib/features/error/models/BaseError.ts
@@ -0,0 +1,14 @@
+export abstract class BaseError extends Error {
+    readonly status: number;
+    readonly source: string;
+    readonly code: string;
+    readonly message: string;
+
+    constructor(status: number, source: string, code: string, message: string) {
+        super();
+        this.status = status;
+        this.source = source;
+        this.code = code;
+        this.message = message;
+    }
+}
