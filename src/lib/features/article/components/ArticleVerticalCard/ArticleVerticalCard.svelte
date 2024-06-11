fe94b7cfd264e1d33943aa7aab7947bf6c11e6ab
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-11 12:38:22 +0000

    add : ブックマークのアイコンを追加

--- src/lib/features/article/components/ArticleVerticalCard/ArticleVerticalCard.svelte
+++ src/lib/features/article/components/ArticleVerticalCard/ArticleVerticalCard.svelte
@@ -3,29 +3,59 @@
 	import { Card, Tooltip } from 'flowbite-svelte';
 	import type { Article, ArticleId } from '../../types/type';
 	import TagChip from '$lib/features/tag/components/TagChip/TagChip.svelte';
-	import { BookmarkOutline } from 'flowbite-svelte-icons';
+	import { BookmarkOutline, BookmarkSolid } from 'flowbite-svelte-icons';
 	import { BookmarkRepository } from '../../repositories/localstorage/bookmark';
-	import ArticleContents from '../ArticleContents/ArticleContents.svelte';
+	import { get } from 'svelte/store';
+	import { BookmarkedArticlesIdStore } from '../../stores/bookmarked-articlesID';
+	
 	export let article: Readonly<Article>; //読みこみのみ可能
 
 	// 更新したかを判定
 	$: wasUpdated = article.updatedAt.toString() !== article.createdAt.toString();
 
+	// ブックマークされているかを判定
+	let isBookmarked: boolean = get(BookmarkedArticlesIdStore).includes(article.id);
+
 	// ブックマーク処理
 	const bookmarkArticle = (articleId: ArticleId) => {
 		BookmarkRepository.setBookmarkedArticleId(articleId);
+		isBookmarked = true;
 	};
+
+	// ブックマーク解除処理
+	const removeBookmarkedArticle = (articleId: ArticleId) => {
+		BookmarkRepository.deleteBookmarkedArticleId(articleId);
+		isBookmarked = false;
+	};
+
 </script>
 
 <Card href={`/articles/${article.slug}`} img={article.imageUrl} class="cursor-pointer" size="sm">
 	<div class="mb-4 w-full flex justify-between items-center">
 		<TagChip tag={article.tag} />
-		<button on:click|preventDefault={() => bookmarkArticle(article.id)} id="bookmark-icon" class="w-12 flex justify-center items-center rounded-full aspect-square hover:bg-yellow-100">
-			<BookmarkOutline size="lg"/>
-		</button>
-		<Tooltip placement="bottom" triggeredBy="#bookmark-icon" >
-			<span>ブックマークする</span>
-		</Tooltip>
+		{#if isBookmarked}
+			<button
+				on:click|preventDefault={() => removeBookmarkedArticle(article.id)}
+				id="bookmarked-icon"
+				class="w-12 flex justify-center items-center rounded-full aspect-square hover:bg-gray-200"
+			>
+				<BookmarkSolid color="#FFD700" size="lg" />
+			</button>
+			<Tooltip placement="bottom" triggeredBy="#bookmarked-icon">
+				<span>ブックマークをはずす</span>
+			</Tooltip>
+		{:else}
+			<button
+				on:click|preventDefault={() => bookmarkArticle(article.id)}
+				id="bookmark-icon"
+				class="w-12 flex justify-center items-center rounded-full aspect-square hover:bg-yellow-100"
+			>
+				<BookmarkOutline size="lg" />
+			</button>
+			<Tooltip placement="bottom" triggeredBy="#bookmark-icon">
+				<span>ブックマークする</span>
+			</Tooltip>
+		{/if}
 	</div>
 	<h5 id="title" class="mb-10 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
 		{article.title}
