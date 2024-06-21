01b30d8b25b4b4e52e9130616268799df2b20f0a
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-21 03:48:00 +0000

    update : Snackbarのdivタグにrole属性を追加

--- src/lib/components/ui/Snackbar/Snackbar.svelte
+++ src/lib/components/ui/Snackbar/Snackbar.svelte
@@ -18,6 +18,7 @@
 {#if active}
 	<div
 		data-component="snackbar"
+		role="alert"
 		transition:fade={{ duration: 350 }}
 		class="fixed right-8 bottom-8 flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md"
 	>
