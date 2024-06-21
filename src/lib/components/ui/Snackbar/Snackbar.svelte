9c37d056dc384904d1716a46ce7363e1fb3e53d3
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-21 01:48:43 +0000

    add : Snackbarを追加

---
+++ src/lib/components/ui/Snackbar/Snackbar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import { CloseOutline } from "flowbite-svelte-icons";
+	import { fade } from "svelte/transition";
+
+    export let message: string;
+    export let timeout: number = 5000;
+    let active: boolean = true;
+
+    const closeSnackbar = () => {
+        active = false;
+    };
+
+    setTimeout(() => {
+        closeSnackbar();
+    }, timeout);
+
+</script>
+
+<div data-component="snackbar" transition:fade={{ duration: 1000 }} class={`${active ? "fixed right-8 bottom-8" : "hidden"} flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md`}>
+    <p class="text-white py-2">{message}</p>
+    <button on:click|preventDefault={closeSnackbar} class="hover:bg-gray-200 transition-all rounded-sm">
+        <div class="p-2">
+            <CloseOutline color="white" class=""/>
+        </div>
+    </button>
+</div>
+
+<style>
+
+</style>
