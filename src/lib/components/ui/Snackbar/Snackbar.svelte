eea938c7625fe95f0420a0a99e2b7d4da5c7a0a8
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-06-21 02:06:11 +0000

    fix : フェードアウトできるように修正

--- src/lib/components/ui/Snackbar/Snackbar.svelte
+++ src/lib/components/ui/Snackbar/Snackbar.svelte
@@ -1,30 +1,34 @@
 <script lang="ts">
-	import { CloseOutline } from "flowbite-svelte-icons";
-	import { fade } from "svelte/transition";
+	import { CloseOutline } from 'flowbite-svelte-icons';
+	import { fade } from 'svelte/transition';
 
-    export let message: string;
-    export let timeout: number = 5000;
-    let active: boolean = true;
+	export let message: string;
+	export let timeout: number = 5000;
+	let active: boolean = true;
 
-    const closeSnackbar = () => {
-        active = false;
-    };
+	const closeSnackbar = () => {
+		active = false;
+	};
 
-    setTimeout(() => {
-        closeSnackbar();
-    }, timeout);
-
+	setTimeout(() => {
+		closeSnackbar();
+	}, timeout);
 </script>
 
-<div data-component="snackbar" transition:fade={{ duration: 1000 }} class={`${active ? "fixed right-8 bottom-8" : "hidden"} flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md`}>
-    <p class="text-white py-2">{message}</p>
-    <button on:click|preventDefault={closeSnackbar} class="hover:bg-gray-200 transition-all rounded-sm">
-        <div class="p-2">
-            <CloseOutline color="white" class=""/>
-        </div>
-    </button>
-</div>
-
-<style>
-
-</style>
+{#if active}
+	<div
+		data-component="snackbar"
+		transition:fade={{ duration: 350 }}
+		class="fixed right-8 bottom-8 flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md"
+	>
+		<p class="text-white py-2">{message}</p>
+		<button
+			on:click|preventDefault={closeSnackbar}
+			class="group hover:bg-red-200 transition-all rounded-sm"
+		>
+			<div class="p-2">
+				<CloseOutline color="white" class="group-hover:text-red-500 transition-all" />
+			</div>
+		</button>
+	</div>
+{/if}
