953b34a246d2f3fdbb92c4e57bdc6b7e6b7431b1
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-08 15:05:17 +0000

    feat : ブックマーク時にSnackbarを出すようにした

--- src/lib/components/ui/Snackbar/Snackbar.svelte
+++ src/lib/components/ui/Snackbar/Snackbar.svelte
@@ -1,35 +1,26 @@
 <script lang="ts">
+	import { SnackbarStore } from '$lib/global-stores/snackbar-store';
 	import { CloseOutline } from 'flowbite-svelte-icons';
 	import { fade } from 'svelte/transition';
 
 	export let message: string;
-	export let timeout: number = 5000;
-	let active: boolean = true;
 
-	const closeSnackbar = () => {
-		active = false;
-	};
-
-	setTimeout(() => {
-		closeSnackbar();
-	}, timeout);
+	const closeSnackbar = () => {};
 </script>
 
-{#if active}
-	<div
-		data-component="snackbar"
-		role="alert"
-		transition:fade={{ duration: 350 }}
-		class="fixed right-8 bottom-8 flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md"
+<div
+	data-component="snackbar"
+	role="alert"
+	transition:fade={{ duration: 350 }}
+	class="fixed right-8 bottom-8 flex box-content bg-sky-500 items-center justify-center gap-x-4 px-5 py-2 rounded-md"
+>
+	<p class="text-white py-2">{message}</p>
+	<button
+		on:click|preventDefault={closeSnackbar}
+		class="group hover:bg-red-200 transition-all rounded-sm"
 	>
-		<p class="text-white py-2">{message}</p>
-		<button
-			on:click|preventDefault={closeSnackbar}
-			class="group hover:bg-red-200 transition-all rounded-sm"
-		>
-			<div class="p-2">
-				<CloseOutline color="white" class="group-hover:text-red-500 transition-all" />
-			</div>
-		</button>
-	</div>
-{/if}
+		<div class="p-2">
+			<CloseOutline color="white" class="group-hover:text-red-500 transition-all" />
+		</div>
+	</button>
+</div>
