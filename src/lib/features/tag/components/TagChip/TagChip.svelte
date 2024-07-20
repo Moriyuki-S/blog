f555ddea524c1daaca02e7e0aa396d53551be15f
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-20 14:49:50 +0000

    style : CSSを調整

--- src/lib/features/tag/components/TagChip/TagChip.svelte
+++ src/lib/features/tag/components/TagChip/TagChip.svelte
@@ -6,7 +6,7 @@
 
 <a href={`/tags/${tag.slug}`}>
 	<div
-		class="w-max h-8 px-4 rounded-lg flex items-center gap-x-2 border hover:text-white hover:bg-primary-600 transition-colors"
+		class="w-max h-8 px-4 rounded-lg font-bold flex items-center gap-x-2 border hover:text-white hover:bg-secondory-600 transition-colors"
 	>
 		#
 		<span class="text-xs">{tag.name}</span>
