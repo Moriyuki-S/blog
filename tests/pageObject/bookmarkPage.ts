fb885d7f4496e52f64446f59929a4304c5292829
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-21 11:41:37 +0000

    add : ブックマーク記事一覧のPOMを作成

---
+++ tests/pageObject/bookmarkPage.ts
@@ -0,0 +1,19 @@
+import type { Locator, Page } from "@playwright/test";
+
+export class BookmarkPage {
+    readonly page: Page;
+    private readonly _resetBookmarkButton: Locator;
+
+    constructor(page: Page) {
+        this.page = page;
+        this._resetBookmarkButton = page.getByRole('button', { name: '解除する' });
+    }
+
+    async goto() {
+        await this.page.goto('/articles/bookmark');
+    }
+
+    async resetBookmark() {
+        await this._resetBookmarkButton.click();
+    }
+}
