8c02ff5af0266769dc9db13c9ed2aead63cb1524
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-22 10:43:32 +0000

    format : フォーマットを修正

--- src/lib/features/article/utils/sort.ts
+++ src/lib/features/article/utils/sort.ts
@@ -1,16 +1,16 @@
-import type { Article } from "../types/type";
+import type { Article } from '../types/type';
 
 // 記事の作成順（更新順ではない）でソート
 const sortByDate = (a: Article, b: Article, criteria: 'latest' | 'oldest'): 1 | -1 => {
-    if (criteria === 'oldest') {
-        return a.createdAt > b.createdAt ? 1 : -1;
-    }
+	if (criteria === 'oldest') {
+		return a.createdAt > b.createdAt ? 1 : -1;
+	}
 
-    return a.createdAt < b.createdAt ? 1 : -1;
+	return a.createdAt < b.createdAt ? 1 : -1;
 };
 
 const SortUtils = {
-    sortByDate
+	sortByDate
 };
 
 export default SortUtils;
