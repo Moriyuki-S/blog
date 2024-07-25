a94cbcde125b38ba7bf101bff50003e22cc16e6d
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-25 06:23:42 +0000

    add : SnackbarListを作成

---
+++ src/lib/components/ui/Snackbar/SnackbarList/SnackbarList.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import type { SnackbarType } from "$lib/global-stores/snackbar-store";
+	import Snackbar from "../Snackbar.svelte";
+    export let snackbars: SnackbarType[];
+</script>
+
+<ul class="fixed right-5 bottom-5 flex flex-col-reverse">
+    {#each snackbars as snackbar (snackbar.id)}
+        <li>
+            <Snackbar message={snackbar.message} type={snackbar.type} />
+        </li>
+    {/each}
+</ul>
