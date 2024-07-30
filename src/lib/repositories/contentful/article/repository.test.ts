9273527e5124d043f7cca2cdd8264b2db4bf51b7
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-30 07:55:58 +0000

    test : タグのテストを実行し、以降はテストをスキップするようにした

--- src/lib/repositories/contentful/article/repository.test.ts
+++ src/lib/repositories/contentful/article/repository.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 import { ContentfulArticlesRepository } from './repository';
 
 describe('記事取得処理のテスト', () => {
-	it('IDを指定して記事を取得できる', async () => {
+	it.skip('IDを指定して記事を取得できる', async () => {
 		const articleRepository = new ContentfulArticlesRepository();
 		const article = await articleRepository.getArticleBySlug('test');
 		expect(article).not.toBe(null);
