66b1d1f40fb93a46ca76eab127c121ca6c110467
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-07-30 09:22:30 +0000

    feat : キーワード検索とタグ検索の機能を実装

--- src/lib/repositories/contentful/article/repository.ts
+++ src/lib/repositories/contentful/article/repository.ts
@@ -1,5 +1,6 @@
 import type { IArticlesRepository } from '$lib/features/article/repositories/apis/fetch-articles';
 import type { Article } from '$lib/features/article/types/type';
+import type { Tag } from '$lib/features/tag/types/type';
 import client from '../client';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 
@@ -47,5 +48,112 @@ export class ContentfulArticlesRepository implements IArticlesRepository {
 		} as Article;
 
 		return article;
+	}
+
+	async getArticlesByTag(tag: Tag, excludedArticle?: Article): Promise<Article[]> {
+		if (excludedArticle) {
+			const entries = await this._client.getEntries({
+				'content_type': 'article',
+				'fields.tag.sys.id': tag.id,
+				'fields.slug[ne]': excludedArticle.slug
+			});
+
+			const articles = entries.items.map(item => {
+				return {
+					id: item.sys.id,
+					slug: item.fields.slug as string,
+					title: item.fields.title as string,
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					body: item.fields.body ? documentToHtmlString(item.fields.body) : '',
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					imageUrl: item.fields.thumbnail.fields.file.url,
+					createdAt: new Date(item.sys.createdAt),
+					updatedAt: new Date(item.sys.updatedAt),
+					tag: {
+						id: tag.id,
+						name: tag.name,
+						slug: tag.slug,
+						iconUrl: tag.iconUrl,
+						color: tag.color
+					}
+				} satisfies Article;
+			});
+
+			return articles;
+		}
+
+		const entries = await this._client.getEntries({
+			'content_type': 'article',
+			'fields.tag.sys.id': tag.id
+		});
+
+		const articles = entries.items.map(item => {
+			return {
+				id: item.sys.id,
+				slug: item.fields.slug as string,
+				title: item.fields.title as string,
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				body: item.fields.body ? documentToHtmlString(item.fields.body) : '',
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				imageUrl: item.fields.thumbnail.fields.file.url,
+				createdAt: new Date(item.sys.createdAt),
+				updatedAt: new Date(item.sys.updatedAt),
+				tag: {
+					id: tag.id,
+					name: tag.name,
+					slug: tag.slug,
+					iconUrl: tag.iconUrl,
+					color: tag.color
+				}
+			} satisfies Article;
+		});
+
+		return articles;
+	}
+
+	async getArticlesByKeyword (keyword: string): Promise<Article[]> {
+		const entries = await this._client.getEntries({
+			'content_type': 'article',
+			'order': ['-sys.createdAt'],
+			'query': keyword,
+		});
+
+		const articles = entries.items.map(item => {
+			return {
+			id: item.sys.id,
+			slug: item.fields.slug as string,
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			body: item.fields.body ? documentToHtmlString(item.fields.body) : '',
+			title: item.fields.title as string,
+			imageUrl: item.fields.thumbnail as string,
+			createdAt: new Date(item.sys.createdAt),
+			updatedAt: new Date(item.sys.updatedAt),
+			// 型エラーを一旦無視
+			tag: {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				id: item.fields.tag.sys.id as string,
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				name: item.fields.tag.fields.name as string,
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				slug: item.fields.tag.fields.slug,
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				iconUrl: item.fields.tag.fields.icon.fields.file.url as string,
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				color: item.fields.tag.fields.color as string
+			}
+			} satisfies Article;
+		});
+
+		return articles;
 	}
 }
