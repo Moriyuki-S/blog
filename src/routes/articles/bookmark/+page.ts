8f7c90ce69b88279996f3731a0ae7aaa9de530da
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-02 02:55:36 +0000

    add : ブックマーク記事の取得処理を追加

---
+++ src/routes/articles/bookmark/+page.ts
@@ -0,0 +1,21 @@
+import { browser } from "$app/environment";
+import { BookmarkRepository } from "$lib/features/article/repositories/localstorage/bookmark";
+import type { PageLoad } from "./$types";
+
+export const load: PageLoad = async ({ fetch }) => {
+    if (!browser) return;
+    
+    const bookmarkedArticlesId = BookmarkRepository.getBookmarkedArticlesId();
+    const query = new URLSearchParams();
+
+    bookmarkedArticlesId.forEach(id => {
+        query.append('id', id);
+    });
+
+    const response = await fetch(`/api/articles?${query}`);
+    const articles = await response.json();
+
+    return {
+        articles
+    }
+};
