e8d0a79eaf16b7b80c0aced066b4ba02cb474d71
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-02 02:29:44 +0000

    add : 記事IDからの記事取得apiを作成

---
+++ src/routes/api/articles/+server.ts
@@ -0,0 +1,12 @@
+import { FetchArticlesUseCase } from "$lib/features/article/application/usecases/fetch-articles";
+import { ContentfulArticlesRepository } from "$lib/repositories/contentful/article/repository";
+import type { RequestHandler } from "@sveltejs/kit";
+
+export const GET: RequestHandler = async ({ url }) => {
+    const articlesID = url.searchParams.getAll('id');
+    const contentfulArticlesRepository = new ContentfulArticlesRepository();
+    const fetchArticlesUseCase = new FetchArticlesUseCase(contentfulArticlesRepository);
+    const articles = await fetchArticlesUseCase.getArticlesByIds(articlesID);
+
+    return new Response(JSON.stringify(articles));
+}
