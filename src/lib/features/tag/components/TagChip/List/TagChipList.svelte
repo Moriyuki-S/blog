21522b2972af998145634c14442fb44d7557a4fc
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-07 05:54:08 +0000

    fix : Cardの一部のみにリンクを張ってリンクのネストを回避するようにした

--- src/lib/features/tag/components/TagChip/List/TagChipList.svelte
+++ src/lib/features/tag/components/TagChip/List/TagChipList.svelte
@@ -4,6 +4,7 @@
 	import TagChip from '../TagChip.svelte';
 
 	export let tags: Tag[];
+	export let shouldfilter: boolean = false;
 
 	const tagsLength = tags.length;
 	const maxDisplayTagsLength = 2;
@@ -12,28 +13,38 @@
 	const notDisplayTags = tags.slice(maxDisplayTagsLength);
 </script>
 
-<ul class="flex gap-x-2">
-	{#each displayTags as tag}
-		<li>
-			<TagChip {tag} />
-		</li>
-	{/each}
-	{#if tagsLength > maxDisplayTagsLength}
-		<li>
-			<button
-				id="show-other-tags-button"
-				class="border-2 w-8 h-8 bg-gray-100 hover:bg-gray-200 hover:border-gray-200 text-sm rounded-full"
-			>
-				<span>+{tagsLength - maxDisplayTagsLength}</span>
-			</button>
-			<Dropdown>
-				{#each notDisplayTags as tag}
-					<DropdownItem class="hover:bg-secondory-600 hover:text-white transition-colors">
-						<a href={`/tags/${tag.slug}`}># {tag.name}</a>
-					</DropdownItem>
-				{/each}
-			</Dropdown>
-			<Tooltip triggeredBy="#show-other-tags-button" placement="right">全てのタグを表示</Tooltip>
-		</li>
-	{/if}
-</ul>
+{#if shouldfilter}
+	<ul class="flex gap-x-2">
+		{#each displayTags as tag}
+			<li>
+				<TagChip {tag} />
+			</li>
+		{/each}
+		{#if tagsLength > maxDisplayTagsLength}
+			<li>
+				<button
+					id="show-other-tags-button"
+					class="border-2 w-8 h-8 bg-gray-100 hover:bg-gray-200 hover:border-gray-200 active:bg-gray-300 active:border-gray-300 text-sm rounded-full"
+				>
+					<span>+{tagsLength - maxDisplayTagsLength}</span>
+				</button>
+				<Dropdown>
+					{#each notDisplayTags as tag}
+						<DropdownItem class="hover:bg-secondory-600 hover:text-white transition-colors">
+							<a href={`/tags/${tag.slug}`}># {tag.name}</a>
+						</DropdownItem>
+					{/each}
+				</Dropdown>
+				<Tooltip triggeredBy="#show-other-tags-button" placement="right">全てのタグを表示</Tooltip>
+			</li>
+		{/if}
+	</ul>
+{:else}
+	<ul class="flex gap-x-2">
+		{#each tags as tag}
+			<li>
+				<TagChip {tag} />
+			</li>
+		{/each}
+	</ul>
+{/if}
