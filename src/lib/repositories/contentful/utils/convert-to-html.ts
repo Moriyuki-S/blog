5c3a0f688ccb00c9c0dd2cf54a329cfa1f963d80
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-07 01:27:58 +0000

    add : 埋め込み系のnodeを作成

--- src/lib/repositories/contentful/utils/convert-to-html.ts
+++ src/lib/repositories/contentful/utils/convert-to-html.ts
@@ -1,6 +1,7 @@
+import { formatTimetoJapanese } from '$lib/utils/time-format';
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import type { Document } from '@contentful/rich-text-types';
-import { BLOCKS } from '@contentful/rich-text-types';
+import { BLOCKS, INLINES } from '@contentful/rich-text-types';
 
 export const convertRichTextToHtml = (richText: Document): string => {
 	return documentToHtmlString(richText, {
@@ -22,6 +23,47 @@ export const convertRichTextToHtml = (richText: Document): string => {
 				const text = node.content[0].value;
 				const id = text.toLowerCase().replace(/ /g, '-');
 				return `<a href="#${id}"><h3 id="${id}">${text}</h3></a>`;
+			},
+			[BLOCKS.HEADING_4]: (node) => {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				const text = node.content[0].value;
+				const id = text.toLowerCase().replace(/ /g, '-');
+				return `<a href="#${id}"><h3 id="${id}">${text}</h3></a>`;
+			},
+			[BLOCKS.QUOTE]: (node) => {
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				const text = node.content[0].content[0].value;
+				return `<blockquote>${text}</blockquote>`;
+			},
+			[INLINES.HYPERLINK]: (node) => {
+				const { uri } = node.data;
+				// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+				// @ts-ignore
+				const text = node.content[0].value;
+				return `<a href="${uri}" class="hyperlink" target="_blank" rel="noopener noreferrer">
+				${text}<svg class="inline-block w-4 h-4 -translate-y-[1.8px] text-primary-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/></svg>
+				</a>`
+			},
+			[INLINES.EMBEDDED_ENTRY]: (node) => {
+				const { title, slug } = node.data.target.fields;
+				return `<a href="/articles/${slug}" class="hyperlink" target="_blank" rel="noopener noreferrer">
+				${title}<svg class="inline-block w-4 h-4 -translate-y-[1.8px] text-primary-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 14v4.833A1.166 1.166 0 0 1 16.833 20H5.167A1.167 1.167 0 0 1 4 18.833V7.167A1.166 1.166 0 0 1 5.167 6h4.618m4.447-2H20v5.768m-7.889 2.121 7.778-7.778"/></svg>
+				</a>`
+			},
+			[BLOCKS.EMBEDDED_ENTRY]: (node) => {
+				const { title, slug, thumbnail } = node.data.target.fields;
+				const { updatedAt } = node.data.target.sys;
+				const formatUpdatedAt = formatTimetoJapanese(new Date(updatedAt));
+				const thumbnailUrl = thumbnail.fields.file.url;
+				return `<a href="/articles/${slug}" target="_blank" class="article-link flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+				<img class="thumbnail w-full rounded-t-lg !max-h-32 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="${thumbnailUrl}" alt="${title}">
+				<div class="flex flex-col justify-between px-4 leading-normal">
+					<h5 class="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">${title}</h5>
+					<time class="ml-auto">投稿: ${formatUpdatedAt}</time>
+				</div>
+			</a>`
 			}
 		}
 	});
