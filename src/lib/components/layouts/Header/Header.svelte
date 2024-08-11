2f2aa0cb6353e7ecadb40c7bedc5129cab8d9bf3
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-11 14:38:52 +0000

    style : スマホでもヘッダーを表示するように変更

--- src/lib/components/layouts/Header/Header.svelte
+++ src/lib/components/layouts/Header/Header.svelte
@@ -16,13 +16,13 @@
 	} from 'flowbite-svelte-icons';
 </script>
 
-<header class="hidden md:block w-full border-b fixed top-0 left-0 z-10">
+<header class="w-full border-b fixed top-0 left-0 z-10">
 	<Navbar aria-labelledby="desktop-navigation">
 		<NavBrand href="/">
 			<h1>ブログ</h1>
 		</NavBrand>
 		<NavHamburger />
-		<div class="flex gap-x-5 items-center">
+		<div class="hidden md:flex gap-x-5 items-center">
 			<NavUl>
 				<NavLi href="/profile" class="inline-flex gap-x-1 items-center">
 					<UserOutline />
