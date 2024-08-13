cb326f1a248f6ff00d24e66d6f34302fc1f26e3c
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-13 14:10:52 +0000

    fix : snackbarの位置を調整

--- src/lib/components/ui/Snackbar/SnackbarList/SnackbarList.svelte
+++ src/lib/components/ui/Snackbar/SnackbarList/SnackbarList.svelte
@@ -5,7 +5,7 @@
 	export let snackbars: SnackbarType[];
 </script>
 
-<ul class="fixed right-5 bottom-5 flex flex-col-reverse gap-y-4 items-end">
+<ul class="fixed bottom-32 left-3 z-50 md:right-5 md:bottom-5 flex flex-col-reverse gap-y-4 items-start md:items-end">
 	{#each snackbars as snackbar (snackbar.id)}
 		<li animate:flip>
 			<Snackbar message={snackbar.message} type={snackbar.type} />
