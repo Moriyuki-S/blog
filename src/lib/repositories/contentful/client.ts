9fd380c0f744e0ba6dca175cc4dd58c43201b9c8
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-08-14 15:06:02 +0000

    add : debug用のコードを追加

--- src/lib/repositories/contentful/client.ts
+++ src/lib/repositories/contentful/client.ts
@@ -1,5 +1,8 @@
 import contentful from 'contentful';
 
+console.log('CONTENTFUL_SPACE_ID:', import.meta.env.CONTENTFUL_SPACE_ID);
+console.log('CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN:', import.meta.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN);
+
 const client = contentful.createClient({
 	space: import.meta.env.CONTENTFUL_SPACE_ID,
 	accessToken: import.meta.env.CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN
