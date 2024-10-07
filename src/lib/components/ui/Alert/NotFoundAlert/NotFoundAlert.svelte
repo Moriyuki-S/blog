e96ddff9d29fa04f825fd41b8a9a1d1a39a6e2a6
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-07 12:00:53 +0000

    add : NotFoundコンポーネントを作成

---
+++ src/lib/components/ui/Alert/NotFoundAlert/NotFoundAlert.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Alert } from "flowbite-svelte";
+	import { InfoCircleSolid } from "flowbite-svelte-icons";
+
+    export let notFoundMessage: string;
+    export let styleClass: string = "";
+
+
+</script>
+
+<Alert border color="red" class={styleClass}>
+    <InfoCircleSolid slot="icon" />
+    <span class="font-bold">{notFoundMessage}</span>
+</Alert>
