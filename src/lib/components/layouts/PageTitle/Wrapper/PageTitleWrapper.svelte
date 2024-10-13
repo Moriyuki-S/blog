bf57bdbdf022e04ffb338f74603b4ffaff320d6d
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-13 04:45:11 +0000

    add : PageTitleのコンポーネントを追加

---
+++ src/lib/components/layouts/PageTitle/Wrapper/PageTitleWrapper.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let styleClass: string = '';
+    export let center: boolean = false;
+    $: centerStyleClass = center ? 'flex justify-center items-center': '';
+</script>
+
+<div class={`w-full h-24 px-2 md:h-48 ${centerStyleClass} ${styleClass}`} >
+    <slot />
+</div>
