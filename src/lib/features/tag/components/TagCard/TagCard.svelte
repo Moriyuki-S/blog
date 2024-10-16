bd54532f4ff5e0ba2520fc421d8932f6c80deb6e
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-16 12:21:32 +0000

    change : tagCardをリンクとボタンの使い分けができるように変更

--- src/lib/features/tag/components/TagCard/TagCard.svelte
+++ src/lib/features/tag/components/TagCard/TagCard.svelte
@@ -2,16 +2,31 @@
 	import type { Tag } from '../../types/type';
 
 	export let tag: Tag;
+	export let onClick: (() => void) | 'link' = 'link';
 </script>
 
-<a
-	href={`/tags/${tag.slug}`}
-	class="flex justify-center items-center w-32 h-32 transition-all border hover:bg-gray-200 hover:bg-opacity-70 hover:-translate-y-1 rounded-lg p-2 md:p-5"
->
-	<div class="flex flex-col items-center justify-center gap-y-5">
-		<div>
-			<img src={tag.iconUrl} alt={`${tag.name}のアイコン`} width="50" height="50" />
+{#if onClick === 'link'}
+	<a
+		href={`/tags/${tag.slug}`}
+		class="flex justify-center items-center w-32 h-32 transition-all border hover:bg-gray-200 hover:bg-opacity-70 hover:-translate-y-1 rounded-lg p-2 md:p-5"
+	>
+		<div class="flex flex-col items-center justify-center gap-y-5">
+			<div>
+				<img src={tag.iconUrl} alt={`${tag.name}のアイコン`} width="50" height="50" />
+			</div>
+			<h3 class="px-2 w-fit">{tag.name}</h3>
 		</div>
-		<h3 class="px-2 w-fit">{tag.name}</h3>
-	</div>
-</a>
+	</a>
+{:else}
+	<button
+		on:click|preventDefault={onClick}
+		class="flex justify-center items-center w-32 h-32 transition-all border hover:bg-gray-200 hover:bg-opacity-70 hover:-translate-y-1 rounded-lg p-2 md:p-5"
+	>
+		<div class="flex flex-col items-center justify-center gap-y-5">
+			<div>
+				<img src={tag.iconUrl} alt={`${tag.name}のアイコン`} width="50" height="50" />
+			</div>
+			<h3 class="px-2 w-fit">{tag.name}</h3>
+		</div>
+	</button>
+{/if}
