bec48e3b9a65a1c5a97963949e4c0b23a463b192
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-16 16:07:10 +0000

    fix : TagFilterDialogのUIを修正

--- src/lib/features/tag/components/TagFilterDialog/TagFilterDialog.svelte
+++ src/lib/features/tag/components/TagFilterDialog/TagFilterDialog.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-	import { Modal, Search } from 'flowbite-svelte';
+	import { Button, Modal, Search, Avatar } from 'flowbite-svelte';
 	import type { Tag } from '../../types/type';
 	import TagHorizontalButton from '../TagHorizontalButton/TagHorizontalButton.svelte';
+	import { TagSolid } from 'flowbite-svelte-icons';
 
 	export let open: boolean = false;
 	export let tags: Tag[];
@@ -24,23 +25,19 @@
 		}
 	}
 
-    const handleSelectTag = () => {
-        selectTag(tag);
-        closeDialog();
-    };
+	const handleSelectTag = (tag: Tag) => {
+		selectTag(tag);
+		closeDialog();
+	};
 
-    const handleResetTag = () => {
-        resetTag();
-        closeDialog();
-    };
-
+	const handleResetTag = () => {
+		resetTag();
+		closeDialog();
+	};
 </script>
 
 <Modal bind:open title="タグで絞る">
 	<div class="p-5">
-        <div>
-            {currentFilterTag?.name}
-        </div>
 		<div>
 			<Search
 				placeholder="タグを検索する"
@@ -50,17 +47,41 @@
 		</div>
 		<div class="min-h-60 mt-8 flex flex-col gap-y-5 overflow-scroll">
 			<div class="border-b pb-2">
-				<button on:click|preventDefault={handleResetTag} class="text-red-500"
-					>すべてのタグ</button
+				<button
+					on:click|preventDefault={handleResetTag}
+					class={`flex items-center font-bold border-2 w-full px-4 py-2 rounded-full ${!currentFilterTag ? 'bg-red-500 text-white border-gray-200' : 'text-red-500 border-red-400'}`}
 				>
+					<TagSolid class="w-6 h-6 me-2" />
+					すべてのタグ
+				</button>
 			</div>
 			<ul class="grid gap-3 grid-cols-auto h-64 overflow-y-scroll">
 				{#each filteredTags as tag}
 					<li>
-                        <TagHorizontalButton {tag} onClick={() => handleSelectTag(tag)} />
+						<TagHorizontalButton
+							active={currentFilterTag?.id === tag.id}
+							{tag}
+							onClick={() => handleSelectTag(tag)}
+						/>
 					</li>
 				{/each}
 			</ul>
 		</div>
+	</div>
+	<div slot="footer" class="w-full flex items-center justify-between">
+		<div class="w-36 overflow-x-auto p-2 border-r pr-4">
+			{#if currentFilterTag}
+				<button class="w-full rounded-lg flex items-center gap-x-3">
+					<Avatar src={currentFilterTag.iconUrl} alt={currentFilterTag.name} />
+					<p class="font-bold whitespace-nowrap">{currentFilterTag.name}</p>
+				</button>
+			{:else}
+				<p class="text-red-500 font-bold whitespace-nowrap">
+					<TagSolid class="w-6 h-6 me-2" />
+					すべてのタグ
+				</p>
+			{/if}
+		</div>
+		<Button color="alternative" on:click={closeDialog}>キャンセル</Button>
 	</div>
 </Modal>
