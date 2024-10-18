9782ffbdb79506d12646fae0a7033dc37d0f1cc1
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-18 09:02:49 +0000

    format : フォーマットを修正

--- src/lib/features/tag/components/TagCard/TagCard.svelte
+++ src/lib/features/tag/components/TagCard/TagCard.svelte
@@ -13,7 +13,14 @@
 	>
 		<div class="flex flex-col items-center justify-center gap-y-5">
 			<div>
-				<ImageWithLoading imageSkeletonClass="w-14" imageSkeletonHeight="50" src={tag.iconUrl} alt={`${tag.name}のアイコン`} width="50" height="50" />
+				<ImageWithLoading
+					imageSkeletonClass="w-14"
+					imageSkeletonHeight="50"
+					src={tag.iconUrl}
+					alt={`${tag.name}のアイコン`}
+					width="50"
+					height="50"
+				/>
 			</div>
 			<h3 class="px-2 w-fit">{tag.name}</h3>
 		</div>
