235f53826ae622bf131e91ecc40da2c830a1ada6
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-29 09:07:52 +0000

    style: TagFilterDialogのスタイルを調整

--- src/lib/features/tag/components/TagFilterDialog/TagFilterDialog.svelte
+++ src/lib/features/tag/components/TagFilterDialog/TagFilterDialog.svelte
@@ -68,13 +68,13 @@
 			</ul>
 		</div>
 	</div>
-	<div slot="footer" class="w-full flex items-center justify-between">
-		<div class="w-36 overflow-x-auto p-2 border-r pr-4">
+	<div slot="footer" class="w-full flex flex-col items-center justify-between sm:flex-row">
+		<div class="w-full sm:w-36 overflow-x-auto p-2 pb-4 sm:pb-2 -mt-2 md:mt-0 border-b sm:border-b-none sm:border-r pr-4">
 			{#if currentFilterTag}
-				<button class="w-full rounded-lg flex items-center gap-x-3">
+				<div class="w-full rounded-lg flex items-center gap-x-3">
 					<Avatar src={currentFilterTag.iconUrl} alt={currentFilterTag.name} />
 					<p class="font-bold whitespace-nowrap">{currentFilterTag.name}</p>
-				</button>
+				</div>
 			{:else}
 				<p class="text-red-500 flex items-center font-bold whitespace-nowrap">
 					<TagSolid class="w-6 h-6 me-2" />
@@ -82,6 +82,6 @@
 				</p>
 			{/if}
 		</div>
-		<Button color="alternative" on:click={closeDialog}>キャンセル</Button>
+		<Button color="alternative" class="mt-5 ml-auto sm:mt-0" on:click={closeDialog}>キャンセル</Button>
 	</div>
 </Modal>
