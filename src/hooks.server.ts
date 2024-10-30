c8e33af7aaa0ee4982f80fd4beb57c28ad0abf3e
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-10-30 11:23:34 +0000

    format: フォーマットを修正

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,17 @@
-import type { HandleServerError } from "@sveltejs/kit";
+import type { HandleServerError } from '@sveltejs/kit';
 
 export const handleError: HandleServerError = async ({ status }) => {
-    if (status === 404) {
-        return {
-            code: '404',
-            source: 'server',
-            message: 'ページが見つかりませんでした',
-        };
-    }
+	if (status === 404) {
+		return {
+			code: '404',
+			source: 'server',
+			message: 'ページが見つかりませんでした'
+		};
+	}
 
-    return {
-        code: '500',
-        source: 'server',
-        message: 'サーバーエラーが発生しました',
-    };
+	return {
+		code: '500',
+		source: 'server',
+		message: 'サーバーエラーが発生しました'
+	};
 };
