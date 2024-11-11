c00f5422aed32aadf0a4487a8648a5436eb7a8a5
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-11-11 11:30:26 +0000

    format: フォーマットを修正

--- src/lib/repositories/contentful/client.ts
+++ src/lib/repositories/contentful/client.ts
@@ -1,10 +1,14 @@
 import * as contentful from 'contentful';
-import { CONTENTFUL_SPACE_ID, CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN, CONTENTFUL_ENVIRONMENT } from '$env/static/private';
+import {
+	CONTENTFUL_SPACE_ID,
+	CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
+	CONTENTFUL_ENVIRONMENT
+} from '$env/static/private';
 
 const client = contentful.createClient({
 	space: CONTENTFUL_SPACE_ID,
 	accessToken: CONTENTFUL_MANAGEMENT_API_ACCESS_TOKEN,
-	environment: CONTENTFUL_ENVIRONMENT,
+	environment: CONTENTFUL_ENVIRONMENT
 });
 
 export default client;
