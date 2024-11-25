42ee8af279b7ad50082e72eb05db8b2fe2245a4c
Moriyuki Suzuki <morimori06601011.ne@outlook.jp>
2024-11-25 13:10:24 +0000

    format: フォーマットを修正

--- src/lib/components/ui/DisplayThemeButton/DisplayThemeButton.svelte
+++ src/lib/components/ui/DisplayThemeButton/DisplayThemeButton.svelte
@@ -35,7 +35,10 @@
 		<MoonOutline class="inline w-7 h-7 me-2 pt-1" />
 		Dark
 	</DropdownItem>
-	<DropdownItem class="text-xl font-bold flex items-center" on:click={() => handleChangeDisplayTheme('system')}>
+	<DropdownItem
+		class="text-xl font-bold flex items-center"
+		on:click={() => handleChangeDisplayTheme('system')}
+	>
 		<DesktopPcOutline class="inline w-7 h-7 me-2 pt-1" />
 		System
 	</DropdownItem>
